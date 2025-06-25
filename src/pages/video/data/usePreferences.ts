efdef36ee3c799b1c296c2aa9b628fe4d28cf345
cbingb666 <cbingb666@gmail.com>
2025-06-26 05:38:24 +0800

    feat: Optimize player preferences
    - Disable listening for storage changes in preferences

--- src/pages/video/data/usePreferences.ts
+++ src/pages/video/data/usePreferences.ts
@@ -4,24 +4,31 @@ import { useStorage } from "@vueuse/core";
  * 用户偏好设置
  */
 export const usePreferences = () => {
-	const preferences = useStorage("x-player-preferences", {
-		// 音量
-		volume: 100,
-		// 静音
-		muted: true,
-		// 播放速率
-		playbackRate: 1,
-		// 显示播放列表
-		showPlaylist: false,
-		// 自动加载缩略图
-		autoLoadThumbnails: true,
-		// 禁用HDR
-		disabledHDR: false,
-		// 缩略图采样间隔
-		thumbnailsSamplingInterval: 60,
-		// 自动播放
-		autoPlay: true,
-	});
+	const preferences = useStorage(
+		"x-player-preferences",
+		{
+			// 音量
+			volume: 100,
+			// 静音
+			muted: true,
+			// 播放速率
+			playbackRate: 1,
+			// 显示播放列表
+			showPlaylist: false,
+			// 自动加载缩略图
+			autoLoadThumbnails: true,
+			// 禁用HDR
+			disabledHDR: false,
+			// 缩略图采样间隔
+			thumbnailsSamplingInterval: 60,
+			// 自动播放
+			autoPlay: true,
+		},
+		undefined,
+		{
+			listenToStorageChanges: false,
+		},
+	);
 
 	return preferences;
 };
